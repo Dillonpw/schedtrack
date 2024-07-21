f6bcfc03daa89459ec1c3ae8cf5826629c43e02b
dillonpw <dillonpwalsh10@gmail.com>
2024-07-21 11:07:56 -0400

    adding auth

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import Resend from "next-auth/providers/resend"
+import { db } from "./db"
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [Resend],
+})
