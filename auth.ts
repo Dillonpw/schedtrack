16c4abb15338ff48ba11f62a2eae86f90c6db6c2
dillonpw <dillonpwalsh10@gmail.com>
2024-07-22 08:27:09 -0400

    updated sign in and auth function

--- auth.ts
+++ auth.ts
@@ -1,9 +1,14 @@
-import NextAuth from "next-auth"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import Resend from "next-auth/providers/resend"
-import { db } from "./db"
- 
+import NextAuth from 'next-auth';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import Resend from 'next-auth/providers/resend';
+import { db } from './db';
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db),
-  providers: [Resend],
-})
+    adapter: DrizzleAdapter(db),
+    providers: [
+        Resend({
+            apiKey: process.env.AUTH_RESEND_KEY,
+            from: 'Resend <onboarding@resend.dev>',
+        }),
+    ],
+});
