1d90f9a09b7d36acf422d40482875b693a479a1c
dillonpw <dillonpwalsh10@gmail.com>
2024-07-23 15:15:45 -0400

    added adjustable header

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,11 +1,16 @@
+import Header from "@/components/Header";
 import SignIn from "@/components/Sign-in";
 import Link from "next/link"
 
 export default function Page() {
   return (
+    <>
+      <Header />
+
     <main className="flex h-screen flex-col items-center justify-center">
-      <Link href="/" className="absolute left-10 top-10 text-2xl hover:underline"> Back</Link>
+      <Link href="/" className="absolute left-10 top-20 text-2xl hover:underline"> Back</Link>
       <SignIn />
     </main>
+    </>
   );
 }
--- components/Sign-in.tsx
+++ components/Sign-in.tsx
@@ -4,16 +4,19 @@ import { Input } from "./ui/input";
 
 export default function SignIn() {
   return (
-    <div className="flex flex-col gap-2 items-center border-2 p-10 rounded-xl bg-muted">
-      <h1 className="text-3xl font-bold pb-5">Sign in</h1>
-      <form className="flex flex-col gap-2 items-center"
+    <div className="mt-[-10rem] flex flex-col items-center gap-2 rounded-xl border-2 bg-muted p-10">
+      <h1 className="pb-5 text-3xl font-bold">Sign in</h1>
+      <form
+        className="flex flex-col items-center gap-2"
         action={async (formData) => {
           "use server";
           await signIn("resend", formData);
         }}
       >
         <Input type="text" name="email" placeholder="Email" />
-        <Button size="signin" type="submit">Signin with Email</Button>
+        <Button size="signin" type="submit">
+          Signin with Email
+        </Button>
       </form>
       <p className="text-center font-semibold">or</p>
       <form
@@ -22,7 +25,9 @@ export default function SignIn() {
           await signIn("google");
         }}
       >
-        <Button size="signin" type="submit">Signin with Google</Button>
+        <Button size="signin" type="submit">
+          Signin with Google
+        </Button>
       </form>
     </div>
   );
