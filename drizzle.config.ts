47df82ea923ade7ede154a68e279568a2864bf8a
dillonpw <dillonpwalsh10@gmail.com>
2024-07-26 07:45:15 -0400

    reinstalled packages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env.local' });
+config({ path: ".env.local" });
 
 export default defineConfig({
-  schema: './db/schema.ts',
-  out: './migrations',
-  dialect: 'postgresql',
+  schema: "./db/schema.ts",
+  out: "./migrations",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
   },
