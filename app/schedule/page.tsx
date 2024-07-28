a77d48b19cef7d3994551e84ca29dda21d8942a2
dillonpw <dillonpwalsh10@gmail.com>
2024-07-28 15:53:36 -0400

    moved logic to component out of page added space

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,91 +1,34 @@
-import { db } from "@/db/index";
 import Header from "@/components/Header";
-import { scheduleEntries } from "@/db/schema";
-import { eq } from "drizzle-orm";
 import { auth } from "@/auth";
-import {
-  Table,
-  TableCaption,
-  TableHeader,
-  TableRow,
-  TableBody,
-  TableCell,
-} from "@/components/ui/table";
-
-interface ScheduleEntry {
-  date: string;
-  dayOfWeek: string;
-  shift: string;
-}
-
-export default async function SchedulePage() {
+import SignIn from "@/components/Sign-in";
+import Link from "next/link";
+import AccountInfo from "@/components/AccountInfo";
+import ScheduleData from "@/components/ScheduleData";
+export default async function Dashboard() {
   const session = await auth();
-  if (!session || !session.user?.id) {
-    return null;
-  }
 
-  const scheduleEntriesData: ScheduleEntry[] = await db
-    .select()
-    .from(scheduleEntries)
-    .where(eq(scheduleEntries.userId, session.user.id));
-
-  if (!scheduleEntriesData || scheduleEntriesData.length === 0) {
+  if (!session?.user)
     return (
       <main>
         <Header />
-        <h1 className="pb-5 text-center text-3xl font-bold">
-          Generated Schedule
-        </h1>
-        <div className="mt-4">
-          <p className="text-center text-lg">No schedule available</p>
+        <Link
+          href="/"
+          className="absolute left-10 top-20 text-2xl hover:underline"
+        >
+          {" "}
+          Back
+        </Link>
+        <div className="flex h-screen flex-col items-center justify-center">
+          <SignIn />
         </div>
       </main>
     );
-  }
-
-  const renderTableView = (schedule: ScheduleEntry[]): JSX.Element => (
-    <Table>
-      <TableCaption className="mb-5">End of List</TableCaption>
-      <TableHeader>
-        <TableRow className="flex w-full justify-between px-4">
-          <TableHeader className="w-[33%] text-left text-xl font-semibold">
-            Day of Week
-          </TableHeader>
-          <TableHeader className="w-[33%] text-center text-xl font-semibold">
-            Date
-          </TableHeader>
-          <TableHeader className="w-[33%] text-right text-xl font-semibold">
-            Shift
-          </TableHeader>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {schedule.map((entry, index) => (
-          <TableRow key={index} className="flex w-full justify-between">
-            <TableCell className="w-[33%] text-left">
-              {entry.dayOfWeek}
-            </TableCell>
-            <TableCell className="w-[33%] text-center">{entry.date}</TableCell>
-            <TableCell className="w-[33%] text-right">{entry.shift}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
 
   return (
     <main>
       <Header />
-      <h1 className="pb-5 text-center text-3xl font-bold">
-        Generated Schedule
-      </h1>
-      <div className="mt-4">
-        {scheduleEntriesData.length > 0 ? (
-          renderTableView(scheduleEntriesData)
-        ) : (
-          <p className="text-center text-lg">No schedule available</p>
-        )}
-      </div>
+      <AccountInfo />
+      <ScheduleData />
     </main>
   );
 }
---
+++ components/ScheduleData.tsx
@@ -0,0 +1,90 @@
+import { db } from "@/db/index";
+import Header from "@/components/Header";
+import { scheduleEntries } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { auth } from "@/auth";
+import {
+  Table,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+
+interface ScheduleEntry {
+  date: string;
+  dayOfWeek: string;
+  shift: string;
+}
+
+export default async function ScheduleData() {
+  const session = await auth();
+  if (!session || !session.user?.id) {
+    return null;
+  }
+
+  const scheduleEntriesData: ScheduleEntry[] = await db
+    .select()
+    .from(scheduleEntries)
+    .where(eq(scheduleEntries.userId, session.user.id));
+
+  if (!scheduleEntriesData || scheduleEntriesData.length === 0) {
+    return (
+      <main>
+        <Header />
+        <h1 className="pb-5 text-center text-3xl font-bold">
+          Generated Schedule
+        </h1>
+        <div className="mt-4">
+          <p className="text-center text-lg">No schedule available</p>
+        </div>
+      </main>
+    );
+  }
+
+  const renderTableView = (schedule: ScheduleEntry[]): JSX.Element => (
+    <Table>
+      <TableCaption className="mb-5">End of List</TableCaption>
+      <TableHeader>
+        <TableRow className="flex w-full justify-between px-4">
+          <TableHeader className="w-[33%] text-left text-xl font-semibold">
+            Day of Week
+          </TableHeader>
+          <TableHeader className="w-[33%] text-center text-xl font-semibold">
+            Date
+          </TableHeader>
+          <TableHeader className="w-[33%] text-right text-xl font-semibold">
+            Shift
+          </TableHeader>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {schedule.map((entry, index) => (
+          <TableRow key={index} className="flex w-full justify-between">
+            <TableCell className="w-[33%] text-left">
+              {entry.dayOfWeek}
+            </TableCell>
+            <TableCell className="w-[33%] text-center">{entry.date}</TableCell>
+            <TableCell className="w-[33%] text-right">{entry.shift}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+
+  return (
+    <section className='mx-10'>
+      <h1 className="pb-5 text-center text-3xl font-bold">
+        Generated Schedule
+      </h1>
+      <div className="mt-4">
+        {scheduleEntriesData.length > 0 ? (
+          renderTableView(scheduleEntriesData)
+        ) : (
+          <p className="text-center text-lg">No schedule available</p>
+        )}
+      </div>
+    </section>
+  );
+}
