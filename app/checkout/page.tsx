4fab2e2533c9b92b62891bc2326f1449ec2977a0
dillonpw <dillonpwalsh10@gmail.com>
2024-07-29 20:00:42 -0400

    beginning to add stripe integration

---
+++ app/checkout/page.tsx
@@ -0,0 +1,10 @@
+import Checkout from '@/components/stripe';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to our store</h1>
+      <Checkout />
+    </div>
+  );
+}
