950c4890392e910545b57828c3cb5e110be031d5
dillonpw <dillonpwalsh10@gmail.com>
2024-08-03 11:09:59 -0400

    adsense

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -7,7 +7,6 @@ const GoogleAdSense = () => {
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
     />
   );
 };
