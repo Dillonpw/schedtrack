20e5daccd8bdc896ec897aba44f5340b7d4c8068
dillonpw <dillonpwalsh10@gmail.com>
2024-08-16 15:45:56 -0400

    updated from placeholder to brand name

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -8,27 +8,26 @@ import {
 export default function Faq() {
   return (
     <div className="flex w-full flex-col gap-6 bg-muted py-12 md:py-24 lg:py-20 xl:py-48">
-      <h2 className="text-center text-2xl font-bold md:text-4xl mb-10">
+      <h2 className="mb-10 text-center text-2xl font-bold md:text-4xl">
         Frequently Asked Questions
       </h2>
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
           <AccordionTrigger className="flex w-full justify-center text-xl">
-            What are the benefits of using Rotating Schedule Builder?
+            What are the benefits of using Sched Track?
           </AccordionTrigger>
           <AccordionContent className="px-5 text-lg md:px-20 md:text-center">
-            Rotating Schedule Builder helps professionals manage their schedules
-            more efficiently.
+            Sched Track helps professionals manage their schedules more
+            efficiently.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
           <AccordionTrigger className="flex w-full justify-center text-xl">
-            How does Rotating Schedule Builder work?
+            How does Sched Track work?
           </AccordionTrigger>
           <AccordionContent className="px-5 text-lg md:px-20 md:text-center">
-            Rotating Schedule Builder allows professionals to create
-            personalized schedules that fit their unique shift patterns and
-            preferences.
+            Sched Track allows professionals to create personalized schedules
+            that fit their unique shift patterns and preferences.
           </AccordionContent>
         </AccordionItem>
 
@@ -37,7 +36,7 @@ export default function Faq() {
             Is this app cluttered with features I won't use?
           </AccordionTrigger>
           <AccordionContent className="px-5 text-lg md:px-20 md:text-center">
-            Rotating Schedule Builder has one job, and it does it well.
+            Sched Track has one job, and it does it well.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-4">
@@ -45,8 +44,7 @@ export default function Faq() {
             Does it cost am arm and a leg?
           </AccordionTrigger>
           <AccordionContent className="px-5 text-lg md:px-20 md:text-center">
-            Rotating Schedule Builder costs nothing. it's completely free for
-            now!
+            Sched Track costs nothing. it's completely free for now!
           </AccordionContent>
         </AccordionItem>
       </Accordion>
