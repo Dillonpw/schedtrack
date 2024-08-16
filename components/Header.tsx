e7f9ad6858c5f547a8bfae3d8429cf238e54443b
dillonpw <dillonpwalsh10@gmail.com>
2024-08-16 15:45:39 -0400

    added bottom border

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 
 const Header = ({ children }: HeaderProps) => {
   return (
-    <header className="flex h-14 items-center justify-between px-4 lg:px-6">
+    <header className="flex h-14 items-center justify-between px-4 lg:px-6 border-b border-2">
       <Link
         href="/"
         className="flex items-center justify-center"
