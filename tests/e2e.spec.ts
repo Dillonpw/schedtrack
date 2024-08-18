0b11ea48bd7b0d364f8fb6ea187d13c7fb10d2f8
dillonpw <dillonpwalsh10@gmail.com>
2024-08-18 12:07:26 -0400

    updated source email for acc creation

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -41,31 +41,19 @@ test.describe("Home Page", () => {
     await expect(page).toHaveURL("https://buy.stripe.com/7sIaFa7EQeJzbW8aEG");
   });
 
-  //sign in with email testing waiting for domain approval from resend
-  test("sign in and account creation", async ({ page }) => {
-    await page.getByRole("link", { name: "Sign In" }).click();
-    await expect(page).toHaveURL("http:/localhost:3000/signin");
-    await page.getByPlaceholder("Email").click();
-    await page.getByPlaceholder("Email").fill("dillonpwalsh10@gmail.com");
-    await page.getByRole("button", { name: "Sign in with Email" }).click();
-  });
+  //sign in with email testing waiting for domain approval from resend 
+  // Working text commented to stop spam
 
-  test("sign in with Google", async ({ page }) => {
-    const email = process.env.TEST_EMAIL as string;
-    const pw = process.env.TEST_PW as string;
-    const signOutBtn = page.locator(`[data-testid="sign-out"]`);
+  // test("sign in and account creation", async ({ page }) => {
+  //   const email = process.env.TEST_EMAIL as string;
+  //   const password = process.env.TEST_PASSWORD as string;
+  //   await page.getByRole("link", { name: "Sign In" }).click();
+  //   await expect(page).toHaveURL("http:/localhost:3000/signin");
+  //   await page.getByPlaceholder("Email").click();
+  //   await page.getByPlaceholder("Email").fill(email);
+  //   await page.getByRole("button", { name: "Sign in with Email" }).click();
+  // });
 
-    await page.getByRole("link", { name: "Sign In" }).click();
-    await page.getByRole("button", { name: "Sign in with Google" }).click();
-    await page.getByLabel("Email or phone").click();
-    await page.getByLabel("Email or phone").fill("dillonwtest@gmail.com");
-    await page.getByRole("button", { name: "Next" }).click();
-    await page.getByLabel("Enter your password").fill("Fs-302121");
-    await page.getByRole("button", { name: "Next" }).click();
-    await page.getByRole("button", { name: "Continue" }).click();
-    await expect(page).toHaveURL("http:/localhost:3000/dashboard");
-    await expect(signOutBtn).toBeVisible();
-  });
 
   //after sign in verify account info is visible
 
@@ -73,18 +61,8 @@ test.describe("Home Page", () => {
 
   test("contact page allows for message send", async ({ page }) => {
     const contactLink = page.locator('[data-testid="contactLink"]');
-    const result = page.locator(`[data-testid="contact"]`);
 
     await contactLink.click();
     await expect(page).toHaveURL("http:/localhost:3000/contact");
-    await page.getByPlaceholder("Anonymous").click();
-    await page.getByPlaceholder("Anonymous").fill("test");
-    await page.getByPlaceholder("5gqoQ@example.com").click();
-    await page.getByPlaceholder("5gqoQ@example.com").fill("dillonwtest0@gmail.com");
-    await page.getByPlaceholder("Message").click();
-    await page.getByPlaceholder("Message").fill("test message");
-    await page.getByRole("button", { name: "Submit Form" }).click();
-    await expect(result).toBeVisible();
-    await expect(result).toHaveText("Form Submitted Successfully");
   });
 });
