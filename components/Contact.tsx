7cd13bbc499b0c09c21edaef8df7639d996b662b
dillonpw <dillonpwalsh10@gmail.com>
2024-09-08 08:00:22 -0400

    cut links from contact page

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -15,47 +15,6 @@ export default function ContactForm() {
       <p className="mx-auto text-center text-xl md:text-2xl">
         Connect with the developer at schedtrackhelp@gmail.com
       </p>
-      <div className="flex space-x-8">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <a
-                id="xLink"
-                href="https://x.com/dillonpw_"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="redirect to X"
-              >
-                <FontAwesomeIcon
-                  className="h-8 w-8 hover:scale-110"
-                  icon={faXTwitter}
-                />
-              </a>
-            </TooltipTrigger>
-            <TooltipContent>Twitter/X</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              {" "}
-              <a
-                id="githubLink"
-                href="https://github.com/Dillonpw/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="redirect to Github"
-              >
-                <FontAwesomeIcon
-                  className="h-8 w-8 hover:scale-110"
-                  icon={faGithub}
-                />
-              </a>
-            </TooltipTrigger>
-            <TooltipContent>Github</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
     </div>
   );
 }
