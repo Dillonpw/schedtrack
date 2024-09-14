33c7dab7ec5ed1836697f57e978812383b97b75c
dillonpw <dillonpwalsh10@gmail.com>
2024-09-13 20:25:09 -0400

    deleted empty file

--- tests/failing.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test.describe("Basic Interactions, designed to show error handling", () => {
-  test.beforeEach(
-    async ({ page }) => await page.goto("https://www.schedtrack.com/"),
-  );
-});
