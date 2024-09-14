53260dc0887c3e860c06db93ff05931e908a16d1
dillonpw <dillonpwalsh10@gmail.com>
2024-09-13 20:20:36 -0400

    directing failing tests to live page

--- tests/failing.spec.ts
+++ tests/failing.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Basic Interactions, designed to show error handling", () => {
   test.beforeEach(
-    async ({ page }) => await page.goto("http://localhost:3000/"),
+    async ({ page }) => await page.goto("https://www.schedtrack.com/"),
   );
 });
