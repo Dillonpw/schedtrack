ea340f11513489dddf861386a261e41cb28bc763
dillonpw <dillonpwalsh10@gmail.com>
2024-09-25 18:08:06 -0400

    adjusted margins, hid paid pricing component

--- app/layout.tsx
+++ app/layout.tsx
@@ -167,7 +167,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <SessionProvider>{children}</SessionProvider>
         </Providers>
         <Link
-          className="m-4 mt-20 flex justify-center text-lg hover:underline"
+          className="m-4 mt-10 flex justify-center text-lg hover:underline"
           href="https://buy.stripe.com/7sIaFa7EQeJzbW8aEG"
         >
           Help Us Keep The Lights On 💡
--- components/pricing.tsx
+++ components/pricing.tsx
@@ -13,7 +13,7 @@ export default async function Pricing() {
       data-testid="pricing"
       className="mx-auto flex max-w-6xl flex-col items-center justify-center gap-6 px-4 py-20 md:py-24 lg:py-32 xl:py-48"
     >
-      <div className="flex w-full justify-center">
+      <div className="flex max-w-[70%] flex-col justify-center gap-4 md:max-w-full md:flex-row">
         <div className="rounded-lg border-2 bg-background p-4 text-sm shadow-lg dark:border dark:border-gray-600 md:p-10">
           <h3 className="mb-4 text-center text-xl font-bold md:text-4xl lg:text-5xl">
             Free Account
@@ -31,7 +31,7 @@ export default async function Pricing() {
               <span>Unlimited schedule adjustments and redos available </span>
             </li>
             <li className="flex items-center gap-2">
-              <span>Schedule any length up to one year in the future</span>
+              <span>Schedule any length up to 6 months into the future</span>
             </li>
             <li className="flex items-center gap-2">
               <span>
@@ -48,6 +48,40 @@ export default async function Pricing() {
             </Button>
           </div>
         </div>
+       {/* <div className="rounded-lg border-2 bg-background p-4 text-sm shadow-lg dark:border dark:border-gray-600 md:p-10">
+          <h3 className="mb-4 text-center text-xl font-bold md:text-4xl lg:text-5xl">
+            Free Account
+          </h3>
+          <p className="mb-8 text-center text-muted-foreground">
+            Start Scheduling
+          </p>
+          <ul className="mb-8 space-y-3">
+            <li className="flex items-center gap-2">
+              <span>
+                Full access to our custom schedule generating technology
+              </span>
+            </li>
+            <li className="flex items-center gap-2">
+              <span>Unlimited schedule adjustments and redos available </span>
+            </li>
+            <li className="flex items-center gap-2">
+              <span>Schedule any length up to 6 months into the future</span>
+            </li>
+            <li className="flex items-center gap-2">
+              <span>
+                Your most recent schedule will always be a few clicks away
+              </span>
+            </li>
+          </ul>
+          <div className="flex items-center justify-between gap-2">
+            <div className="font-bold md:text-2xl lg:text-5xl">FREE</div>
+            <Button asChild variant="default">
+              <Link href="/signin" prefetch={false}>
+                Get Started
+              </Link>
+            </Button>
+          </div>
+        </div> */}
       </div>
     </section>
   );
