765d5db66dfde8a78482c710dd4bc2f5a79ea911
dillonpw <dillonpwalsh10@gmail.com>
2024-12-27 12:07:37 -0500

    you can zoom if you need to but shouldnt need to

--- app/layout.tsx
+++ app/layout.tsx
@@ -201,7 +201,6 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false,
 };
 
 // Google Ads script generation
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -19,7 +19,6 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false,
 };
 
 export default async function Dashboard() {
