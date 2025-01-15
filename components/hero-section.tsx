41f751afad93a2d619d8c0528a688ef7bf6dc960
Dillon Walsh <dillonpwalsh10@gmail.com>
2025-01-14 19:26:06 -0500

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -8,7 +8,7 @@ const HeroSection = ({ children }: { children: React.ReactNode }) => {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
-    <section className="w-full bg-muted py-40 md:pb-60 md:pt-40">
+    <section className="w-full bg-muted pb-40 pt-20 md:pb-60 md:pt-40">
       <div className="container px-4 md:px-16">
         <div className="flex flex-col items-center justify-center sm:flex-row">
           <div className="flex flex-col justify-center space-y-4">
