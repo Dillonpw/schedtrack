38c59dcadb1f726533dfc2e4753ae238ede842a1
dillonpw <dillonpwalsh10@gmail.com>
2025-04-29 17:36:13 -0400

    skeleton loading in account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import AccountManagement from "@/components/account-management";
 import AccountStatistics from "@/components/account-statistics";
 import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function AccountPage() {
   if (!process.env.DATABASE_URL) {
@@ -18,7 +19,17 @@ export default async function AccountPage() {
     <main>
       <div className="container mx-auto p-8">
         <h1 className="mb-8 text-2xl font-bold">Account Management</h1>
-        <Suspense fallback={<div>Loading statistics...</div>}>
+        <Suspense
+          fallback={
+            <div className="mb-8 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <Skeleton className="h-[76px] w-full md:col-span-2 lg:col-span-4" />
+              <Skeleton className="h-[126px] w-full" />
+              <Skeleton className="h-[126px] w-full" />
+              <Skeleton className="h-[126px] w-full" />
+              <Skeleton className="h-[126px] w-full" />
+            </div>
+          }
+        >
           <AccountStatistics />
         </Suspense>
         <AccountManagement />
